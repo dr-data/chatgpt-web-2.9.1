525c368a9cdba5f35160349b8bc3c90b0cd82cf0
Yc <1412006675@qq.com>
2023-03-03 16:32:13 +0800

    更改网站

---
+++ src/components/common/Setting/About.vue
@@ -0,0 +1,54 @@
+<script setup lang='ts'>
+import { onMounted, ref } from 'vue'
+import { NSpin } from 'naive-ui'
+import { fetchChatConfig } from '@/api'
+
+interface ConfigState {
+  timeoutMs?: number
+  reverseProxy?: string
+  apiModel?: string
+  socksProxy?: string
+}
+
+const loading = ref(false)
+
+const config = ref<ConfigState>()
+
+async function fetchConfig() {
+  try {
+    loading.value = true
+    const { data } = await fetchChatConfig<ConfigState>()
+    config.value = data
+  }
+  finally {
+    loading.value = false
+  }
+}
+
+onMounted(() => {
+  fetchConfig()
+})
+</script>
+
+<template>
+  <NSpin :show="loading">
+    <div class="p-4 space-y-4">
+      <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
+
+
+					<p style="text-align:center;">
+						如果你觉得这个网站对你有帮助，可以微信扫码给予一点赞助，谢谢！
+						<a href="https://i.postimg.cc">
+							<img src="https://i.postimg.cc/Vkq0v1Bc/image.png" alt="扫码赞助" style="display: block; margin: 0 auto;width: 200px; height: 200px;">
+						</a>
+					</p>
+
+
+      </div>
+      <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
+      <p>{{ $t("setting.reverseProxy") }}：{{ config?.reverseProxy ?? '-' }}</p>
+      <p>{{ $t("setting.timeout") }}：{{ config?.timeoutMs ?? '-' }}</p>
+      <p>{{ $t("setting.socks") }}：{{ config?.socksProxy ?? '-' }}</p>
+    </div>
+  </NSpin>
+</template>
